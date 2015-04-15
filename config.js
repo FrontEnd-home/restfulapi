ca2fe8efcb71dfd83f3ef6b3ab7fe842bd5dc0b5
cloudyan <cloudyan@qq.com>
2015-04-15 17:02:30 +0800

    update

--- config.js
+++ config.js
@@ -1,9 +1,10 @@
 "use strict";
 
 exports.debug = true;
-exports.port = 3000;
+exports.port = 3010;
 exports.email = '630284783@qq.com';
 exports.site_name = 'FrontEnd';
 exports.site_desc = 'FrontEnd';
 
-exports.db = 'mongodb://xhzhu:123456@172.16.178.26:27017/FrontEnd';
+//exports.db = 'mongodb://xhzhu:123456@172.16.178.44:27017/FrontEnd';
+exports.db = 'mongodb://localhost:27017/FrontEnd';
